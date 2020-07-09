484ca3abc5a7fe1c6b089386c1d7f76a7fb525cf
lee-wood <leemaewood@gmail.com>
2020-07-08 21:44:16 -0400

    hello and bignum problems

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,7 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+print(2**65536)
+
+# in JS, this would result in an "Infinity" message rather than returning the actual number.
+  
