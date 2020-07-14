3b6211d55b0cf937150b89ec4e59c27262e5c981
lee-wood <leemaewood@gmail.com>
2020-07-14 16:52:42 -0400

    file completed

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,11 +9,17 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
-# YOUR CODE HERE
+f = open(r'C:\Users\Aleesha\LAMBDA_PROJECTS\cs\Intro-Python-I\src\foo.txt', 'r') 
+print( f.read() )
+f.close()
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
+# * What if this was a method that could take in user inputs...?? 
 
-# YOUR CODE HERE
+newFile = open(r'C:\Users\Aleesha\LAMBDA_PROJECTS\cs\Intro-Python-I\src\bar.txt', 'w')
+newFile.write("Hi friends...I'm the new file on the block.")
+newFile.close()
