2ff0a27ced0735f86f8de2f5258a443a2c4f1784
lee-wood <leemaewood@gmail.com>
2020-07-14 18:11:01 -0400

    extra exercise on fil_io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -18,8 +18,32 @@ f.close()
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
-# * What if this was a method that could take in user inputs...?? 
 
 newFile = open(r'C:\Users\Aleesha\LAMBDA_PROJECTS\cs\Intro-Python-I\src\bar.txt', 'w')
 newFile.write("Hi friends...I'm the new file on the block.")
 newFile.close()
+
+
+
+# * What if this was a function that used inputs...?? 
+"""
+- takes in two arguments(name of new file and the text for the file)
+- should be coming from inputs rather than parameters on the functions. Two inputs.
+- open/create file...need to dynamically add file name to the path on `open()`...format string method?
+- succuss message.
+"""
+
+
+def newTxtFile():
+    name = input("File name: ")
+    txt = input("File text: ")
+
+    f = open(r'C:\Users\Aleesha\LAMBDA_PROJECTS\cs\Intro-Python-I\src\{0}.txt'.format(name), 'w')
+    f.write(txt)
+    f.close()
+    
+    print( "file created!" )
+    
+newTxtFile()
+
+#!This works!! 
